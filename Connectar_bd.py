2ffbc322ef66468a9335c88893ca594b658d7caf
Monica Vazquez <msvazquezmereles@alumnos.unsada.edu.ar>
2026-04-11 15:08:03 -0300

    Base de datos en la nube

--- Connectar_bd.py
+++ Connectar_bd.py
@@ -1,14 +1,18 @@
-import mysql.connector 
+import mysql.connector
 
-def obtener_conexion():
-    try:
-        return mysql.connector.connect(
-            host="localhost",
-            user="root", 
-            password="MoniVaz1002@",
-            database="corre_electronico"
-        )
-    except mysql.connector.Error as err:
-        print(f"Error de conexión: {err}")
-        return None
+try:
+    conexion = mysql.connector.connect(
+        host="bfjm6tvgjghm0h0dprcw-mysql.services.clever-cloud.com",
+        user="u7f21txv45ys4qul",
+        password="u03SnyKrk1LWMVymzhcK",
+        database="bfjm6tvgjghm0h0dprcw",
+        port=3306
+    )
     
+    if conexion.is_connected():
+        print("¡Conexión exitosa a la base de datos en la nube!")
+        # Importante: recordá crear el cursor dentro de tus funciones
+        # cursor = conexion.cursor()
+
+except mysql.connector.Error as err:
+    print(f"Error al conectar: {err}")
